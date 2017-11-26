85803506d233a4fa6625c624bbdc3abf1fc75f79
Joacim Breiler <joacim@nimlab.se>
2017-11-26 21:23:10 +0100

    Changed the swagger base documentation.

--- src/main/java/com/breiler/contribe/config/SwaggerConfig.java
+++ src/main/java/com/breiler/contribe/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package com.breiler.contribe.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -16,9 +18,19 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(getApiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.breiler"))
                 .paths(regex("/.*"))
                 .build();
+    }
+
+    private ApiInfo getApiInfo() {
+        return new ApiInfo("REST api documentation for book store",
+                "This is the REST api documentation for a book store. It has support for creating books and managing its stock. A user can also create a cart and fill it with books and be used to create an order.",
+                "1.0",
+                "urn:tos",
+                (Contact) null,
+                "GPLv3", "https://www.gnu.org/licenses/gpl-3.0.en.html");
     }
 }
