ff0b6c076ad27a7e34c11cc6e67610a078ad86ce
Joacim Breiler <joacim@nimlab.se>
2017-11-26 21:19:35 +0100

    Added SQLite as a DBMS for the application

---
+++ src/main/java/com/breiler/contribe/config/DatabaseConfig.java
@@ -0,0 +1,18 @@
+package com.breiler.contribe.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+    @Bean
+    public DataSource dataSource() {
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.sqlite.JDBC");
+        dataSourceBuilder.url("jdbc:sqlite:bookstore.db");
+        return dataSourceBuilder.build();
+    }
+}
